9ad181e394ac30f82ec6be46f31e36162359c7cb
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-19 16:34:33 -0700

    Created file '101-quote' for '0x00' advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <unistd.h> 
+
+/* Prints a string to stderr using write() instead of printf() or puts() */
+int main(void)
+{
+	const char str[] =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, (const void *) str, sizeof(str) * sizeof(char));
+	return (1);
+}
