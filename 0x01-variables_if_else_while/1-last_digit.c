2feff56f10ea731614f6701a2b02786d21f68d67
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-20 12:45:48 -0700

    Fixed modulo 10 not being performed on n in 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,9 +17,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	printf("Last digit of %d is %d %s\n", n, n % 10,
-		(n > 5) ? "and is greater than 5"
-			: ((n == 0) ? "and is 0"
-				    : "and is less than 6 and not 0"));
+		((n % 10) == 0) ? "and is 0"
+			: (((n % 10) > 5) ? "and is greater than 5"
+				: "and is less than 6 and not 0"));
 
 	return (0);
 }
