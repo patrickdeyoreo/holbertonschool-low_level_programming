ec1ce28d8904595a9591fa0ddb4786a357131021
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-20 15:01:25 -0700

    Fixed missing space between after for keyword in '3-print_alphabets.c'

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int c;
 
-	for(c = 0; c < 26; ++c)
+	for (c = 0; c < 26; ++c)
 		putchar('a' + c);
-	for(c = 0; c < 26; ++c)
+	for (c = 0; c < 26; ++c)
 		putchar('A' + c);
 
 	putchar('\n');
