a11c67f42a57aec11851e175536c133ce819d2a1
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-19 17:37:10 -0700

    Fixed style in 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
-#include <stdio.h> 
-#include <unistd.h> 
+#include <stdio.h>
+#include <unistd.h>
 
-/* Prints a string to stderr using write() instead of printf() or puts() */
+/*
+ * Prints a string to stderr using the write function
+ * main - entry point
+ * returns 1
+ */
 int main(void)
 {
-	const char str[] =
+	const char s[] =
 		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, (const void *) str, sizeof(str) * sizeof(char) - 1);
+	write(STDERR_FILENO, (const void *) s, sizeof(s) * sizeof(char) - 1);
 	return (1);
 }
