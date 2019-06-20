54e9afaf30ab39c211f13e7f19038aa6b1a55c9a
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-20 16:14:34 -0700

    Committing first attempt at 10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,9 +1,33 @@
+#include <stdio.h>
+
 /**
  * main - entry point
  *
- * Returns: 0
-*/
+ * Description: Write a program that prints the numbers from 00 to 99
+ * Return: 0
+ */
 int main(void)
 {
+	int digits[2] = { 0, 0 };
+	int sep[2] = { ',', ' ' };
+	int i = 0;
+
+	for (digits[0] = 0; digits[0] < 10; ++(digits[0]))
+	{
+		for (digits[1] = 0; digits[1] < 10; ++(digits[1]))
+		{
+			for (i = 0; i < 2; ++i)
+				putchar('0' + digits[i]);
+
+			if (digits[0] != 9 || digits[1] != 9)
+			{
+				for (i = 0; i < 2; ++i)
+					putchar(sep[i]);
+			}
+		}
+	}
+
+	putchar('\n');
+
 	return (0);
 }
