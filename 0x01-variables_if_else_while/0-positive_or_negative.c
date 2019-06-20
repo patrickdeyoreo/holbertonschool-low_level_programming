bf928786df090813967bc37f7de9257d251fb5bf
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-20 12:05:42 -0700

    Commiting first attempt for 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - entry point
  *
@@ -5,5 +9,15 @@
 */
 int main(void)
 {
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	printf("Last digit of %d is %s\n", n % 10,
+		(n > 5) ? "and is greater than 5"
+			: ((n == 0) ? "and is 0"
+				    : "and is less than 6 and not 0"));
+
 	return (0);
 }
