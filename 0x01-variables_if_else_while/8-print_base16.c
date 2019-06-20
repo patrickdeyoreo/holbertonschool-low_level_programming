0a6732fb2f675854c3a441f015ab372a1c1dfcf8
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-20 15:46:17 -0700

    Committing first attempt of 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,21 @@
+#include <stdio.h>
+
 /**
  * main - entry point
  *
- * Returns: 0
-*/
+ * Description: Print all the digits of base 16 in lowercase
+ * Return: 0
+ */
 int main(void)
 {
+	char x;
+
+	for (x = '0'; x <= '9'; ++x)
+		putchar(x);
+	for (x = 'a'; x <= 'f'; ++x)
+		putchar(x);
+
+	putchar('\n');
+
 	return (0);
 }
