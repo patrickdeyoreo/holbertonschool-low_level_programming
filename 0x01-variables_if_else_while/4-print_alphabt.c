1a910759542c24139136df1b4ddca87c1114c445
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-20 15:20:12 -0700

    Commiting first attempt at 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,9 +1,21 @@
+#include <stdio.h>
 /**
  * main - entry point
  *
- * Returns: 0
-*/
+ * Description: Print lowercase alphabet excluding e and q
+ * Return: 0
+ */
 int main(void)
 {
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	}
+
+	putchar('\n');
+
 	return (0);
 }
