6b46ebe8151474e9c3e987e85d42cf4322719cdd
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-19 18:46:45 -0700

    Added function documentation to 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(int argc, const char *argv[])
+/**
+ * main - entry point
+ * Description: Print a string using followed by a new line
+ * Return: 0
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
