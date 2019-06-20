a2174a77a644e11d16a271c79d7fa6818c4b515b
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-20 15:40:52 -0700

    Commiting first attempt at 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,19 @@
+#include <stdio.h>
+
 /**
  * main - entry point
  *
- * Returns: 0
-*/
+ * Description: Print lowercase alphabet backwards
+ * Return: 0
+ */
 int main(void)
 {
+	char c;
+
+	for (c = 'z'; c >= 'a'; --c)
+		putchar(c);
+
+	putchar('\n');
+
 	return (0);
 }
