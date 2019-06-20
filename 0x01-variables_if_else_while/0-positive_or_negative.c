bd75033695a8528f1172f181df9816f239e66391
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-20 12:09:01 -0700

    Swapped incorrectly ordered files 0-positive_or_negative.c and 1-last_digit.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
  * main - entry point
  *
@@ -9,15 +5,5 @@
 */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	printf("Last digit of %d is %s\n", n % 10,
-		(n > 5) ? "and is greater than 5"
-			: ((n == 0) ? "and is 0"
-				    : "and is less than 6 and not 0"));
-
 	return (0);
 }
