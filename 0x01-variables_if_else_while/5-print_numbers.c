ca8736c46fb914d1d917f25fcdcfebb340183af2
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-20 15:26:51 -0700

    Commiting first attempt at 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,9 +1,19 @@
+#include <stdio.h>
+
 /**
  * main - entry point
  *
- * Returns: 0
-*/
+ * Description: Print numbers 0 to 9 on one line
+ * Return: 0
+ */
 int main(void)
 {
+	char i;
+
+	for (i = '0'; i <= '9'; ++i)
+		putchar(i);
+
+	putchar('\n');
+
 	return (0);
 }
