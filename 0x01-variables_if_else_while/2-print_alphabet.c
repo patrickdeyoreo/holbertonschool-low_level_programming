c197b19122b543bd268f3a1d8983c26d5c866de2
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-20 12:50:57 -0700

    Commiting first attempt of 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,13 @@
+#include <stdio.h>
 /**
  * main - entry point
  *
- * Returns: 0
-*/
+ * Return: 0
+ */
 int main(void)
 {
+	for (char alpha = 'a'; alpha <= 'z'; ++alpha)
+		putchar(alpha);
+	putchar('\n');
 	return (0);
 }
