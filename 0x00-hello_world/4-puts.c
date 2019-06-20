4a2a28ea0d52a5ee1f8ded5c1eabddf401bd27b0
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-19 18:39:54 -0700

    Added documentation for main function in 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ * Description: Print a string using followed by a new line
+ * Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
