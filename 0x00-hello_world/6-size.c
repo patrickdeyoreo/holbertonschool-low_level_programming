f764c9921110b457027a0bdcf19e8cabc2240a88
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-19 18:47:54 -0700

    Added function documentation to 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
-/*
- * Prints sizes of various types
+/**
  * main - entry point
- * returns 0
  */
-
 int main(void)
 {
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
