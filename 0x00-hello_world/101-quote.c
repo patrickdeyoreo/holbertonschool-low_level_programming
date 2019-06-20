b2cc94929cf818db5c5cbecae600168d654007ec
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-19 17:45:00 -0700

    Hardcoded 101-quote.c string and string length

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,10 @@
  * main - entry point
  * returns 1
  */
+
 int main(void)
 {
-	const char s[] =
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, (const void *) s, sizeof(s) * sizeof(char) - 1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59);
 	return (1);
 }
