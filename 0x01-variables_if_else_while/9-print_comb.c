b28324b73f0c9139a01eb2b0db6db121c6b473de
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-20 17:19:11 -0700

    Committing first attempt of 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,9 +1,27 @@
+#include <stdio.h>
+
 /**
  * main - entry point
  *
- * Returns: 0
-*/
+ * Description: Print all possible combinations of single-digit numbers
+ * Return: 0
+ */
 int main(void)
 {
+	int i = 0;
+
+	for (i = 0; i < 10; ++i)
+	{
+		putchar('0' + i);
+
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
 	return (0);
 }
