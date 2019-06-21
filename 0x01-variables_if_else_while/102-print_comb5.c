bb5463a10003ddbc4528d673c34c7a02c345e2e4
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-20 17:03:47 -0700

    Committed first attempt at 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print all possible combinations of two two-digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int x1;
+	int x2;
+	int y1;
+	int y2;
+
+	for (x1 = 0; x1 < 10; ++x1)
+	{
+		for (x2 = 0; x2 < 10; ++x2)
+		{
+			for (y1 = x1; y1 < 10; ++y1)
+			{
+				for (y2 = x2 + 1; y2 < 10; ++y2)
+				{
+					putchar('0' + x1);
+					putchar('0' + x2);
+					putchar(' ');
+					putchar('0' + y1);
+					putchar('0' + y2);
+
+					if (x1 != 9 || x2 != 8 ||
+					    y1 != 9 || y2 != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
