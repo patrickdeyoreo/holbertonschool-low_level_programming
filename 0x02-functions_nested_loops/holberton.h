22ef762cca77b3c66025f96bdea1bfd51f1efb31
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-25 15:00:58 -0700

    Adding 0x02-functions_nested_loops/holberton.h

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,42 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <unistd.h>
+
+/* 0 */
+int _putchar(char c);
+
+/* 1 */
+void print_alphabet(void);
+
+/* 2 */
+void print_alphabet_x10(void);
+
+/* 3 */
+int _islower(int c);
+
+/* 4 */
+int _isalpha(int c);
+
+/* 5 */
+int print_sign(int n);
+
+/* 6 */
+int _abs(int);
+
+/* 7 */
+int print_last_digit(int);
+
+/* 8 */
+void jack_bauer(void);
+
+/* 9 */
+void times_table(void);
+
+/* 10 */
+int add(int, int);
+
+/* 11 */
+void print_to_98(int n);
+
+#endif /* HOLBERTON_H */
