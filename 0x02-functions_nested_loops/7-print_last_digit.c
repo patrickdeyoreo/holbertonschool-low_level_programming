15124705fb9c9c9e2137d0b57c2200c8788e173d
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-25 16:21:49 -0700

    Changed ternary expression to if statement in 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,16 @@
 
 /**
  * print_last_digit - print the last digit of an integer
- * @n: The value to operate on
+ * @n: The value to print the last digit of
  *
  * Return: The last digit of n.
  */
 int print_last_digit(int n)
 {
-	n = (n < 0 ? -n : n) % 10;
+	if (n < 0)
+		n *= -1;
+	n %= 10;
+
 	_putchar('0' + n);
 	return (n);
 }
