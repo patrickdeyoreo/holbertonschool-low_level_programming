a1579347a4745a5a8d4266da3e64de3fc4173002
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-25 16:56:28 -0700

    Satisfied betty-style reqs for 0x03-debugging tasks 1, 2 & 3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,30 +10,30 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b)
-    {
-	   if (a > c)
-		   largest = a;
-	   else
-		   largest = c;
-    }
-    else if (b > a)
-    {
-	   if (b > c)
-		   largest = b;
-	   else
-		   largest = c;
-    }
-    else if (a > c)
-    {
-	    largest = a;
-    }
-    else
-    {
-	    largest = c;
-    }
+	if (a > b)
+	{
+		if (a > c)
+			largest = a;
+		else
+			largest = c;
+	}
+	else if (b > a)
+	{
+		if (b > c)
+			largest = b;
+		else
+			largest = c;
+	}
+	else if (a > c)
+	{
+		largest = a;
+	}
+	else
+	{
+		largest = c;
+	}
 
-    return (largest);
+	return (largest);
 }
