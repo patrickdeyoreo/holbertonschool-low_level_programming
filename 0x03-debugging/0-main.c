b9f385f02003da70aea19983229dd99e47fb21ff
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-25 17:01:34 -0700

    Re-read requirements of for 0-main.c and fixed solution

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 98;
-	positive_or_negative(i);
-	positive_or_negative(0);
-	return (0);
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
