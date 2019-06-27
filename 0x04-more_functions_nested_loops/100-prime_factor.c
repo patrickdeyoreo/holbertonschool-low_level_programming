b8c3b445e74b5d01df7c75afd2ceb72769ba9f08
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-27 14:29:53 -0700

    Adding attempt at 0x04 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+
+/**
+ * isprime - check if integer is divisible by any number other than 1.
+ * @n: integer to check
+ *
+ * Return: 1 if n is prime,
+ * otherwise 0.
+ */
+int isprime(long n)
+{
+	long i;
+	long i_max = n / 2;
+
+	for (i = 2; i < i_max; ++i)
+	{
+		if (n % i == 0)
+			return (0);
+	}
+	return (1);
+}
+
+
+/**
+ * largest_prime_factor - find the largest prime_factor of an integer.
+ * @n: integer to factor
+ *
+ * Return: largest prime factor of n.
+ */
+long largest_prime_factor(long n)
+{
+	long i;
+
+	while (!isprime(n))
+	{
+		for (i = 2; n % i || !isprime(i); ++i)
+			;
+		n /= i;
+	}
+	return (n);
+}
+
+
+/**
+ * main - find and print the largest prime factor of 612852475143
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%ld\n", largest_prime_factor(612852475143));
+	return (0);
+}
