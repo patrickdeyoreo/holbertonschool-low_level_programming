1fd2ff9ecf328e6ad7207b68040d9c4801eec7e9
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-27 15:05:44 -0700

    Adding attempt at 0x04 task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_number - print an integer (with no trailing newline)
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n >= 0)
+	{
+		if (n > 9)
+			print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
+	else
+	{
+		_putchar('-');
+		if (n < -9)
+			print_number(-n / 10);
+		_putchar('0' - n % 10);
+	}
+
+}
