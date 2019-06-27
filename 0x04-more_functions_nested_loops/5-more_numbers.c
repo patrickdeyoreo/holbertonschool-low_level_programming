77b691e341e6c7cd6ac6595665823396ab794e29
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-27 13:13:35 -0700

    Fixed 0x04 task 5 to print 0 through 15 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,17 @@
  */
 void more_numbers(void)
 {
-	int i = 0;
+	int i;
+	int j;
 
-	for (i = 0; i < 15; ++i)
+	for (j = 0; j < 10; ++j)
 	{
-		if (i > 9)
-			_putchar('0' + i / 10);
-		_putchar('0' + i % 10);
+		for (i = 0; i < 15; ++i)
+		{
+			if (i > 9)
+				_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
