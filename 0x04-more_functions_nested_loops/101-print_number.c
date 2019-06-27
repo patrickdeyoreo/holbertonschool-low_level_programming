89566c9eb157e4a6288785cf0e556934ed50631f
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-27 15:15:38 -0700

    Changed recursion in 101-print_number.c to print '-' negatives the recurse on the abs value

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,16 +10,14 @@ void print_number(int n)
 {
 	if (n >= 0)
 	{
-		if (n > 9)
+		if (n >= 10)
 			print_number(n / 10);
 		_putchar('0' + n % 10);
 	}
 	else
 	{
 		_putchar('-');
-		if (n < -9)
-			print_number(-n / 10);
-		_putchar('0' - n % 10);
+		print_number(-n);
 	}
 
 }
