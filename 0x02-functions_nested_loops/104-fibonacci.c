1a9930a255c4c81e1d350cf5979522fd0162a415
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-30 02:47:06 -0700

    104-fibonacci looks good and betty is happy

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,12 +11,10 @@ int main(void)
 {
 	int i;
 	int j;
-
 	long a_lo = 1;
 	long b_lo = 2;
 	long a_hi = 0;
 	long b_hi = 0;
-
 	int limit_len = floor(log10(LONG_MAX / 2));
 	long limit = pow(10, limit_len);
 
@@ -25,32 +23,24 @@ int main(void)
 		if (a_hi)
 		{
 			printf("%ld", a_hi);
-
 			for (j = floor(log10(a_lo)) + 1; j < limit_len; ++j)
 				putchar('0');
 		}
-
 		printf("%ld", a_lo);
-
 		b_lo = b_lo + a_lo;
 		a_lo = b_lo - a_lo;
-
 		a_hi = b_hi - a_hi;
 		b_hi = b_hi + a_hi;
-
 		if (b_lo >= limit)
 		{
 			b_hi += b_lo / limit;
 			b_lo %= limit;
-
 			a_hi += a_lo / limit;
 			a_lo %= limit;
 		}
-
 		if (i < 97)
 			printf(", ");
 	}
-
 	putchar('\n');
 
 	return (0);
