d0ba9352528aa2e3fe5d19d575873d32c05cdbb7
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-06-30 02:39:50 -0700

    First 98 fibonacci numbers printed using 64-bit types

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,3 +1,5 @@
+#include <limits.h>
+#include <math.h>
 #include <stdio.h>
 
 /**
@@ -8,18 +10,45 @@
 int main(void)
 {
 	int i;
-	unsigned long int a = 1;
-	unsigned long int b = 2;
+	int j;
+
+	long a_lo = 1;
+	long b_lo = 2;
+	long a_hi = 0;
+	long b_hi = 0;
+
+	int limit_len = floor(log10(LONG_MAX / 2));
+	long limit = pow(10, limit_len);
 
 	for (i = 0; i < 98; ++i)
 	{
-		printf("%lu", a);
-		if (i < 97)
+		if (a_hi)
 		{
-			b += a;
-			a = b - a;
-			printf(", ");
+			printf("%ld", a_hi);
+
+			for (j = floor(log10(a_lo)) + 1; j < limit_len; ++j)
+				putchar('0');
 		}
+
+		printf("%ld", a_lo);
+
+		b_lo = b_lo + a_lo;
+		a_lo = b_lo - a_lo;
+
+		a_hi = b_hi - a_hi;
+		b_hi = b_hi + a_hi;
+
+		if (b_lo >= limit)
+		{
+			b_hi += b_lo / limit;
+			b_lo %= limit;
+
+			a_hi += a_lo / limit;
+			a_lo %= limit;
+		}
+
+		if (i < 97)
+			printf(", ");
 	}
 
 	putchar('\n');
