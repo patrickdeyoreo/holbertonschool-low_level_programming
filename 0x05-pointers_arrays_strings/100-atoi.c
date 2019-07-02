8b96c4c64a6c7eee6d3aaf58d8b9285b62d6d944
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-02 11:15:19 -0700

    Commiting 101-atoi.c with runtime overflow errors fixed

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <limits.h>
 
 /**
  * _atoi - convert a string to an integer
@@ -9,7 +10,7 @@
  */
 int _atoi(char *s)
 {
-	int n = 0;
+	unsigned int n = 0;
 	int sign = 1;
 
 	if (s)
@@ -17,7 +18,19 @@ int _atoi(char *s)
 		while (*s)
 		{
 			if (*s >= '0' && *s <= '9')
+			{
 				n = n * 10 + (*s - '0');
+				if (sign > 0)
+				{
+					if (n >= INT_MAX)
+						return INT_MAX;
+				}
+				else
+				{
+					if ((n >> (sizeof(unsigned int) - 1)))
+						return INT_MIN;
+				}
+			}
 			else if (n)
 				break;
 			else if (*s == '-')
@@ -28,4 +41,3 @@ int _atoi(char *s)
 
 	return (n * sign);
 }
-
