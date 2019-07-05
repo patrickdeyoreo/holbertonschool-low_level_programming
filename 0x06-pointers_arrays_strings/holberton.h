dad4684a378c677417705fe0d9a61a095b3fa6bd
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-05 10:15:41 -0700

    Added prototypes for 0x06 advanced tasks to holberton.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -14,5 +14,8 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif /* HOLBERTON_H */
