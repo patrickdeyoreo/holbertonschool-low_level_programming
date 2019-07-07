09a8be1a2dbaf8767caa59cb16fd09cf06a13718
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-06 23:11:00 -0700

    Eliminated condition in 8-rot13.c by using 6th char bit to preserve case

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -11,13 +11,13 @@
 char *rot13(char *s)
 {
 	char *c;
+	char shift;
 
 	for (c = s; *c; ++c)
 	{
-		if ('a' <= *c && *c <= 'z')
-			*c = (*c - 'a' + 13) % 26 + 'a';
-		else if ('A' <= *c && *c <= 'Z')
-			*c = (*c - 'A' + 13) % 26 + 'A';
+		shift = 'A' + (*c & 32);
+		if (('a' <= *c && *c <= 'z') || ('A' <= *c && *c <= 'Z'))
+			*c = (*c - shift + 13) % 26 + shift;
 	}
 
 	return (s);
