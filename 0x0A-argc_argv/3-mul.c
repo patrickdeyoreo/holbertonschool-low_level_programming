5efe7bdeb23f788e158ce958f277b59ee28c6cb1
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-13 14:05:36 -0700

    Replaced _atoi with stdlib atoi (mine is broken?) & handled negative numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,54 +1,6 @@
 #include "holberton.h"
 #include <limits.h>
 
-/**
- * _atoi - convert a string to an integer
- * @s: the string to convert
- *
- * Return: result of conversion
- */
-int _atoi(char *s)
-{
-	unsigned int n = 0;
-	int sign = 1;
-	int flag = 0;
-
-	while (*s)
-	{
-		if (*s >= '0' && *s <= '9')
-		{
-			flag = 1;
-			if (sign > 0)
-			{
-				if (INT_MAX / 10 < (int) n)
-					return (INT_MAX);
-				n *= 10;
-				if (INT_MAX - (*s - '0') < (int) n)
-					return (INT_MAX);
-			}
-			else
-			{
-				if (INT_MIN / 10 < (int) -n)
-					return (INT_MIN);
-				n *= 10;
-				if (INT_MIN + (*s - '0') > (int) -n)
-					return (INT_MIN);
-			}
-			n += (*s - '0');
-		}
-		else if (flag)
-		{
-			break;
-		}
-		else if (*s == '-')
-		{
-			sign *= -1;
-		}
-		++s;
-	}
-
-	return (n * sign);
-}
 
 /**
  * print_number - print an integer
@@ -73,6 +25,7 @@ void print_number(int n)
 	}
 }
 
+
 /**
  * _puts - print a string, followed by a new line
  * @str: the string to print
@@ -86,8 +39,9 @@ void _puts(char *str)
 	_putchar('\n');
 }
 
+
 /**
- * main - print the name of the program
+ * main - print the product of two numbers
  * @argc: size of the argument vector
  * @argv: program name and arguments
  *
@@ -102,7 +56,7 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	print_number(_atoi(argv[1]) * _atoi(argv[2]));
+	print_number(atoi(argv[1]) * atoi(argv[2]));
 	_putchar('\n');
 
 	return (0);
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,54 +1,6 @@
 #include "holberton.h"
 #include <limits.h>
 
-/**
- * _atoi - convert a string to an integer
- * @s: the string to convert
- *
- * Return: result of conversion
- */
-int _atoi(char *s)
-{
-	unsigned int n = 0;
-	int sign = 1;
-	int flag = 0;
-
-	while (*s)
-	{
-		if (*s >= '0' && *s <= '9')
-		{
-			flag = 1;
-			if (sign > 0)
-			{
-				if (INT_MAX / 10 < (int) n)
-					return (INT_MAX);
-				n *= 10;
-				if (INT_MAX - (*s - '0') < (int) n)
-					return (INT_MAX);
-			}
-			else
-			{
-				if (INT_MIN / 10 < (int) -n)
-					return (INT_MIN);
-				n *= 10;
-				if (INT_MIN + (*s - '0') > (int) -n)
-					return (INT_MIN);
-			}
-			n += (*s - '0');
-		}
-		else if (flag)
-		{
-			break;
-		}
-		else if (*s == '-')
-		{
-			sign *= -1;
-		}
-		++s;
-	}
-	return (n * sign);
-}
-
 
 /**
  * print_number - print an integer
@@ -97,6 +49,8 @@ void _puts(char *str)
  */
 int isnumber(char *s)
 {
+	if (*s == '-')
+		++s;
 	while ('0' <= *s && *s <= '9')
 		++s;
 	return (!*s);
@@ -108,14 +62,14 @@ int isnumber(char *s)
  * @argc: size of the argument vector
  * @argv: program name and arguments
  *
- * Return: 1 if called with anything other than 2 arguments,
+ * Return: 1 if called with non-integer arguments,
  * otherwise 0
  */
 int main(int argc, char *argv[])
 {
 	int sum;
 
-	for (sum = 0; --argc; sum += _atoi(*argv))
+	for (sum = 0; --argc; sum += atoi(*argv))
 	{
 		if (!isnumber(*(++argv)))
 		{
@@ -126,5 +80,6 @@ int main(int argc, char *argv[])
 
 	print_number(sum);
 	_putchar('\n');
+
 	return (0);
 }
