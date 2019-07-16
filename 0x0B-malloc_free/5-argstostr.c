832485bf00584d159082cc535096e878aad2aaf5
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-16 16:20:12 -0700

    Adding first attempt at 0x0B 5-argtostr.c

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,11 +1,38 @@
 #include "holberton.h"
 
 /**
- * argstostr -
+ * argstostr - concatenate all arguments into a new string
  *
- * Return:
+ * Return: NULL memory allocation fails, ac is 0 or av is NULL,
+ * otherwise return a pointer to the new string
  */
 char *argstostr(int ac, char **av)
 {
+	char *cat, *chr;
+	int arg, size;
 
+	if (!ac || !av)
+		return (NULL);
+
+	for (arg = 0, size = 1; arg < ac; ++arg, ++size)
+	{
+		for (chr = av[arg]; *chr; ++chr, ++size)
+			;
+	}
+
+	cat = (char *) malloc(sizeof(char) * size);
+
+	if (!cat)
+		return (NULL);
+
+	for (arg = 0, size = 0; arg < ac; ++arg, ++size)
+	{
+		for (chr = av[arg]; *chr; ++chr, ++size)
+			cat[size] = *chr;
+		cat[size] ='\n';
+	}
+
+	cat[size] = '\0';
+
+	return (cat);
 }
