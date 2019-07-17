b6259aa2249f9a81849467775d94a339ca30a7d4
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-16 23:59:27 -0700

    Commiting updated header and unfinished 100

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,64 @@
+#include "holberton.h"
+
+/**
+ * _isspace - check if a character is whitespace
+ * @c: the character to check
+ *
+ * Return: 1 is c is a whitespace character, otherwise 0
+ */
+int _isspace(int c)
+{
+	if (c == 0x20 || (0x09 <= c && c <= 0x0d))
+		return (1);
+	return (0);
+}
+
+
+/**
+ * strtow - split a string into words
+ * @str: a pointer to the string to split
+ *
+ * Return: NULL if memory allocation fails or if str is NULL or empty (""),
+ * otherwise return a pointer to the array of words terminated by a NULL
+ */
+char **strtow(char *str)
+{
+	char **words, *pos = str;
+	int len = 0, w = 0, c = 0;
+
+	if (!(str && *str))
+		return (NULL);
+
+	do {
+		while (_isspace(*pos))
+			++pos;
+		if (!*pos)
+			break;
+		++w;
+		while (*pos && !_isspace(*pos))
+			++pos;
+	} while (*pos);
+
+	words = (char **) malloc(sizeof(char *) * (w + 1));
+	if (!words)
+		return NULL;
+	pos = str;
+	w = 0;
+	do {
+		while (_isspace(*pos))
+			++pos;
+		if (!*pos)
+			break;
+		for (str = pos; *pos && !_isspace(*pos); ++pos)
+			;
+		words[w] = (char *) malloc(sizeof(char) * (pos - str + 1));
+		if (!words[w])
+			return (NULL);
+		for (c = 0; str < pos; ++c, ++str)
+			words[w][c] = *str;
+		words[w][c] = '\0';
+	} while (++w, *pos);
+	words[w] = NULL;
+
+	return (words);
+}
