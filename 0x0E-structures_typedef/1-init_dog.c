0a48e884a276802e2c73adaa8573e094e375dc6d
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-22 13:59:37 -0700

    Using return in void

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,10 +12,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+	if (!d)
+		return;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
