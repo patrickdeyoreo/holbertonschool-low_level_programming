b9c0d7d41f112713972071edde382714b8148344
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-22 13:40:18 -0700

    Changed 1-init_dog.c to assign char ptrs instead of strdup

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,36 +2,6 @@
 
 
 /**
- * _strdup - create a new array containing a copy of the given string
- * @str: a pointer to the string to copy
- *
- * Return: If str is NULL or memory allocation fails, return NULL.
- * Otherwise return a pointer to the new copy.
- */
-char *_strdup(char *str)
-{
-	char *dup;
-	unsigned int len = 0;
-
-	if (!str)
-		return (NULL);
-
-	while (str[len])
-		++len;
-
-	dup = malloc(sizeof(char) * len + 1);
-	if (!dup)
-		return (NULL);
-
-	do {
-		dup[len] = str[len];
-	} while (len--);
-
-	return (dup);
-}
-
-
-/**
  * init_dog  - initialize a variable of type struct dog
  * @d: a pointer to the struct dog variable
  * @name: the name of the dog
@@ -42,7 +12,7 @@ char *_strdup(char *str)
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = _strdup(name);
+	d->name = name;
 	d->age = age;
-	d->owner = _strdup(owner);
+	d->owner = owner;
 }
