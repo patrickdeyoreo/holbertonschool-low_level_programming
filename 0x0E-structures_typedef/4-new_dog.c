47f8daee71dd367fc3990796594e9f38b9b9fa63
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-22 14:23:48 -0700

    Using _strdup in 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,35 @@
 #include "dog.h"
 
 /**
+ * _strdup - create a new array containing a copy of the given string
+ * @str: a pointer to the string to copy
+ *
+ * Return: NULL if str is NULL or if memory allocation fails,
+ * otherwise a return a pointer to the new copy
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int size = 0;
+
+	if (str)
+	{
+		while (str[size++])
+			;
+
+		dup = malloc(sizeof(char) * size);
+		if (dup)
+		{
+			while (size--)
+				dup[size] = str[size];
+
+			return (dup);
+		}
+	}
+	return (NULL);
+}
+
+/**
  * new_dog - create a new dog
  * @name: the new dog's name
  * @age: the new dog's age
@@ -10,8 +39,9 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d = malloc(sizeof(dog_t));
+	dog_t *d;
 
+	d = malloc(sizeof(dog_t));
 	if (!d)
 		return (NULL);
 
