496858469f3bbb57a2bb66305d9bc05afa41e51e
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-22 13:38:30 -0700

    Adding first attempt at 0x0E task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,11 +1,48 @@
 #include "dog.h"
 
-/**
- * print_dog -
- *
- * Return:
- */
-void print_dog(struct dog *d)
-{
 
+/**
+ * _strdup - create a new array containing a copy of the given string
+ * @str: a pointer to the string to copy
+ *
+ * Return: If str is NULL or memory allocation fails, return NULL.
+ * Otherwise return a pointer to the new copy.
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int len = 0;
+
+	if (!str)
+		return (NULL);
+
+	while (str[len])
+		++len;
+
+	dup = malloc(sizeof(char) * len + 1);
+	if (!dup)
+		return (NULL);
+
+	do {
+		dup[len] = str[len];
+	} while (len--);
+
+	return (dup);
+}
+
+
+/**
+ * init_dog  - initialize a variable of type struct dog
+ * @d: a pointer to the struct dog variable
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner of the dog
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = _strdup(name);
+	d->age = age;
+	d->owner = _strdup(owner);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - attributes of a dog
  * @name: the name of the dog
@@ -13,5 +16,9 @@ struct dog
 	float age;
 	char *owner;
 };
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 
 #endif /* DOG_H */
