e5b14c6bdaa0be9ccc0b76243a7df692315538a3
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-22 14:13:24 -0700

    Typdef'd struct dog as dog_t for 0x0E task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,12 @@ struct dog
 	char *owner;
 };
 
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
