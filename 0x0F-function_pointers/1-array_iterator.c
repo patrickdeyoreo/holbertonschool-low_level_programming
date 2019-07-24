44e71f80c210bf55f265a3e2f0aa92850b144866
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-24 15:37:13 -0700

    Committing first attempt at 0x0F task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,11 +1,15 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator -
+ * array_iterator - perform an action on each element of an array
+ * @array: the array to iterate over
+ * @size: the size of array
+ * @action: the action to perform on each array element
  *
- * Return:
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-
+	while (size--)
+		action(*array++);
 }
