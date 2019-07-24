77a4910e6bf3543b1da8190a54444f7a54986170
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-24 15:46:53 -0700

    Committing first attempt at 0x0F task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,11 +1,26 @@
 #include "function_pointers.h"
 
 /**
- * int_index -
+ * int_index - search for an integer
+ * @array: the array to search
+ * @size: the number of elements in the array
+ * @cmp: the function to use to compare values (returns non-zero upon match)
  *
- * Return:
+ * Return: If no match is found or size is less than 1, return -1. Otherwise
+ * return the index of the first match.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
 
+	if (cmp && array)
+	{
+		for (i = 0; i < size; ++i)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+
+	return (-1);
 }
