379b9613c6c6e6bd6d6b6aef0e7e744bc9a7cdd8
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-24 15:29:42 -0700

    Committing first attempt at 0x0F task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,13 @@
 #include "function_pointers.h"
 
 /**
- * print_name -
+ * print_name - print a name using the function supplied by the caller
+ * @name: the name to print
+ * @f: the function to call to print the name
  *
- * Return:
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-
+	f(name);
 }
