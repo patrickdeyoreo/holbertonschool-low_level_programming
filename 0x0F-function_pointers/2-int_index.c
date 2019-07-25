909525355abf6dcbb8d626f9e4707c605e6308d5
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-24 17:31:12 -0700

    Updated 'Return:' doc in 2-int_index.c and removed calc.h funcs from function_pointers.h

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,10 +4,10 @@
  * int_index - search for an integer
  * @array: the array to search
  * @size: the number of elements in the array
- * @cmp: the function to use to compare values (returns non-zero upon match)
+ * @cmp: the function to use to compare values (returns non-zero upon a match)
  *
- * Return: If no match is found or size is less than 1, return -1. Otherwise
- * return the index of the first match.
+ * Return: If no match is found or size is less than 1, or if array is NULL or
+ * cmp is NULL, return -1. Otherwise return the index of the first match.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
