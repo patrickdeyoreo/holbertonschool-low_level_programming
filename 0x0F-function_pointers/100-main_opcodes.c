79659ee7b9741ddc84c721c31746368e8cf5dbfe
Patrick DeYoreo <pdeyoreo@gmail.com>
2019-07-24 22:26:48 -0700

    Commiting 0x0F task 4 program to print its own opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print opcodes of this function
+ * @argc: the size of the argument vector
+ * @argv: the argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int b;
+	char *mem = (char *) main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	b = atoi(argv[1]);
+
+	if (b < 0)
+	{
+		printf("Error\n");
+		exit (2);
+	}
+
+	while (b--)
+		printf("%02x%c", *mem++ & 0xff, b ? ' ' : '\n');
+
+	return (0);
+}
