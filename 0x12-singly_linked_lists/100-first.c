0d03260c0ece95128972802c1efa4e76ca980771
Patrick DeYoreo <29614644+patrickdeyoreo@users.noreply.github.com>
2019-08-02 23:10:08 -0700

    First advanced task for 0x12

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+/**
+ * init - execute before main
+ */
+void init(void) __attribute__((constructor));
+ 
+void init(void)
+{
+	printf("%s\n%s\n",
+	"You're beat! and yet, you must allow,",
+	"I bore my house upon my back!"
+	);
+}
