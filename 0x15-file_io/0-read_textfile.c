a355cf2491f8e10d4f6435619d7dc508e4a11f1c
patrickdeyoreo <pdeyoreo@gmail.com>
2019-08-13 16:04:07 -0700

    Committing updated holberton.h

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,48 @@
 #include "holberton.h"
 
 /**
- * read_textfile -
+ * read_textfile - read a text file and print it to stdout
+ * @filename: the name of the file to read
+ * @letters: the number of letters to be read and printed
  *
- * Return: 
+ * Return: If filename is NULL, the file cannot be opened or read, or
+ * write fails or returns an unexpected number of bytes, return 0.
+ * Otherwise, return the actual number of letters read and printed.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buffer = NULL;
+	ssize_t b_read;
+	ssize_t b_written;
+	int fd;
 
+	if (!(filename && letters))
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+
+	b_read = read(fd, buffer, letters);
+	close(fd);
+
+	if (b_read < 0)
+	{
+		free(buffer);
+		return (0);
+	}
+	if (!b_read)
+		b_read = letters;
+
+	b_written = write(STDOUT_FILENO, buffer, b_read);
+	free(buffer);
+
+	if (b_written < 0)
+		return (0);
+
+	return (b_written);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,8 +1,10 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
