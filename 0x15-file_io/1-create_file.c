4bb14a49ccbcfb59a4521729d219e3b2ab2ec86c
patrickdeyoreo <pdeyoreo@gmail.com>
2019-08-13 16:39:28 -0700

    Committing first attempt at 0x15 task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,51 @@
 #include "holberton.h"
 
 /**
- * create_file -
+ * _strlen - compute the length of a NULL-terminated string
+ * @str: the string to measure
  *
- * Return: 
+ * Return: the length of str, or -1 if str is NULL
+ */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
+
+	if (!str)
+		return (-1);
+
+	while (*str++)
+		++len;
+
+	return (len);
+}
+
+/**
+ * create_file - create a file
+ * @filename: the name of the file to create
+ * @text_content: the data to write to filename
+ *
+ * Return: Upon success, return 1. Otherwise, return -1.
  */
 int create_file(const char *filename, char *text_content)
 {
+	ssize_t b_written;
+	int fd;
 
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd < 0)
+		return (-1);
+
+	b_written = write(fd, text_content, _strlen(text_content));
+	close(fd);
+
+	if (b_written < 0)
+		return (-1);
+	return (1);
 }
