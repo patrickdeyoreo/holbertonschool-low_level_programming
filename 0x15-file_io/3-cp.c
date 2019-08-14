297405893c945710ef67af8136cc68cd0222d8b3
patrickdeyoreo <pdeyoreo@gmail.com>
2019-08-13 18:15:54 -0700

    Fixed fd error handling in 0x15 task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,22 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define BUFSIZE 1024
+
+/**
+ * _close - close a file descriptor and print an error if it fails
+ * @fd: the file descriptor to close
+ *
+ * Return: 0 upon success, -1 upon failure
+ */
+int _close(int fd)
+{
+	if (!close(fd))
+		return (0);
+
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	return (-1);
+}
 
 /**
  * main - copy a file's contents to another file
@@ -15,7 +31,7 @@ int main(int argc, const char *argv[])
 {
 	int fd_in, fd_out;
 	ssize_t bytes_read;
-	char buffer[1024];
+	char buffer[BUFSIZE];
 
 	if (argc != 3)
 	{
@@ -33,21 +49,20 @@ int main(int argc, const char *argv[])
 	fd_out = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_out < 0)
 	{
+		_close(fd_in);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	while ((bytes_read = read(fd_in, buffer, 1024)))
+	while ((bytes_read = read(fd_in, buffer, BUFSIZE)))
 		write(fd_out, buffer, bytes_read);
-	if (close(fd_in))
+
+	if (_close(fd_in) < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_in);
+		close(fd_out);
 		exit(100);
 	}
-	if (close(fd_out))
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_out);
+	if (_close(fd_out) < 0)
 		exit(100);
-	}
 
 	return (0);
 }
