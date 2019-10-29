90823ef7690d24562ca62f1c7d51e2cd965fbae9
patrickdeyoreo <pdeyoreo@gmail.com>
2019-10-29 01:20:09 -0700

    Fixed insertion sort node swap

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,60 +1,38 @@
 #include "sort.h"
 
 /**
- * swap - Swap two nodes
- * @a: a double pointer to a node
- * @b: a double pointer to a node
- */
-void swap(listint_t *a, listint_t *b)
-{
-	listint_t *temp;
-
-	temp = a->next;
-	a->next = b->next;
-	if (a->next)
-		a->next->prev = a;
-	b->next = temp;
-	if (b->next)
-		b->next->prev = b;
-
-	temp = a->prev;
-	a->prev = b->prev;
-	if (a->prev)
-		a->prev->next = a;
-	b->prev = temp;
-	if (b->prev)
-		b->prev->next = b;
-}
-
-/**
  * insertion_sort_list - Perform the insertion sort alogorithm
  * @list: a double pointer to the head of a list
  */
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr = NULL;
+	listint_t *next = NULL;
 	listint_t *prev = NULL;
 	listint_t *temp = NULL;
 
-	if (!(list && *list))
-		return;
-
-	prev = (*list);
-	curr = (*list)->next;
-
-	while (curr)
+	if (list && *list)
 	{
-		temp = curr;
-		curr = curr->next;
-		while (prev && temp->n < prev->n)
+		next = (*list)->next;
+		while ((curr = next))
 		{
-			print_list(*list);
-			swap(temp, prev);
-			prev = temp->prev;
+			next = curr->next;
+			while ((prev = curr->prev) && prev->n > curr->n)
+			{
+				print_list(*list);
+				temp = prev->prev;
+				prev->prev = curr;
+				curr->prev = temp;
+				if (temp)
+					temp->next = curr;
+				temp = curr->next;
+				curr->next = prev;
+				prev->next = temp;
+				if (temp)
+					temp->prev = prev;
+			}
+			if (!prev)
+				*list = curr;
 		}
-		if (!prev)
-			*list = temp;
-		if (curr)
-			prev = curr->prev;
 	}
 }
