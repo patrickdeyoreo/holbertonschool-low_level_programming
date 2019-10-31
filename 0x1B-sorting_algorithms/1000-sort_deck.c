ff14eb02dca2f67613188716ba431e4f0179f41d
patrickdeyoreo <pdeyoreo@gmail.com>
2019-10-31 01:24:03 -0700

    Bettified deck sorting header and implementation

--- 0x1B-sorting_algorithms/1000-sort_deck.c
+++ 0x1B-sorting_algorithms/1000-sort_deck.c
@@ -1,9 +1,24 @@
 #include "deck.h"
 #include <stdio.h>
 
-int compare(const 
+/**
+ * compare - compare two cards
+ * @card1: a card
+ * @card2: a card
+ *
+ * Return:
+ * -1 if card1 is less than card 2
+ *  1 if card1 is greater than card2
+ *  0 otherwise
+ */
+int compare(const deck_node_t *card1, const deck_node_t *card2)
+{
+}
 
+/**
+ * sort_deck - sort a deck of cards
+ * @deck: a double pointer to the head of a doubly-linked list of cards
+ */
 void sort_deck(deck_node_t **deck)
 {
-    
 }
--- 0x1B-sorting_algorithms/deck.h
+++ 0x1B-sorting_algorithms/deck.h
@@ -3,12 +3,19 @@
 
 #include <stdlib.h>
 
+/**
+ * enum kind_e - mneumonic for suits
+ * @SPADE: spades suit
+ * @HEART: hearts suit
+ * @CLUB: clubs suit
+ * @DIAMOND: diamonds suit
+ */
 typedef enum kind_e
 {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
 } kind_t;
 
 /**
@@ -20,8 +27,8 @@ typedef enum kind_e
  */
 typedef struct card_s
 {
-    const char *value;
-    const kind_t kind;
+	const char *value;
+	const kind_t kind;
 } card_t;
 
 /**
@@ -33,9 +40,9 @@ typedef struct card_s
  */
 typedef struct deck_node_s
 {
-    const card_t *card;
-    struct deck_node_s *prev;
-    struct deck_node_s *next;
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
 } deck_node_t;
 
 void sort_deck(deck_node_t **deck);
