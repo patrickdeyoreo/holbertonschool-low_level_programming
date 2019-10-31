6ab8599440c8f28656b38370911f35c647494bf6
patrickdeyoreo <pdeyoreo@gmail.com>
2019-10-31 01:31:23 -0700

    Added radix_sort to sort.h

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -30,6 +30,7 @@ void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
