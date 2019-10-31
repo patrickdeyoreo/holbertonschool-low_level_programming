57247476cca40503b909d9630fbc522477cad345
akeemseymens <akeem.seymens@holbertonschool.com>
2019-10-31 06:42:34 +0000

    radix added

---
+++ 0x1B-sorting_algorithms/105-radix_sort.c
@@ -0,0 +1,71 @@
+#include "sort.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * find_largest - a function that sorts an array of integers
+ * in ascending order using the Counting sort algorithm
+ * @array: Array to sort
+ * @size: size of Array
+ * Return: Void
+ */
+
+int getMax(int *array, size_t size)
+{
+	size_t i;
+	int mx = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > mx)
+			mx = array[i];
+	}
+	return (mx);
+}
+
+/**
+ * radixsort - a function that sorts an array of integers
+ * in ascending order using the Radix sort algorithm
+ * @array: Array to sort
+ * @size: size of Array
+ * @exp: digit of numbers in array
+ * Return: Void
+ */
+void radixsort(int *array, int size, int exp)
+{
+	int *output;
+	int i;
+	int count[10] = {0};
+
+	output = malloc(sizeof(int) * size);
+	for (i = 0; i < size; i++)
+		count[(array[i] / exp) % 10]++;
+	for (i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+	for (i = size - 1; i >= 0; i--)
+	{
+		output[count[(array[i] / exp) % 10] - 1] = array[i];
+		count[(array[i] / exp) % 10]--;
+	}
+	for (i = 0; i < size; i++)
+		array[i] = output[i];
+	print_array(array, size);
+	free(output);
+}
+
+/**
+ * radix_sort - a function that sorts an array of integers
+ * in ascending order using the Radix sort algorithm
+ * @array: Array to sort
+ * @size: size of Array
+ * Return: Void
+ */
+void radix_sort(int *array, size_t size)
+{
+	int exp, mx = getMax(array, size);
+
+	if (!array || size < 2)
+		return;
+	for (exp = 1; mx / exp > 0; exp *= 10)
+		radixsort(array, size, exp);
+}
