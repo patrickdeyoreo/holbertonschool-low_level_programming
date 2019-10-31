4d9faa246658a0f764aea32975f88407c8344095
patrickdeyoreo <pdeyoreo@gmail.com>
2019-10-31 02:41:02 -0700

    Adding working deck_sort

--- 0x1B-sorting_algorithms/1000-sort_deck.c
+++ 0x1B-sorting_algorithms/1000-sort_deck.c
@@ -1,18 +1,76 @@
 #include "deck.h"
 #include <stdio.h>
 
+#define DECKSIZE 52
+
+/**
+ * strcmp - compare two strings
+ * @s1: a string to compare
+ * @s2: the other string to compare
+ * Return:
+ * an integer < 0 if s1 < s2
+ * an integer > 0 if s1 > s2
+ * 0 otherwise
+ */
+int strcmp(const char *s1, const char *s2)
+{
+	for (; *s1 && *s2; ++s1, ++s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+	}
+
+	if (*s1)
+		return (1);
+	if (*s2)
+		return (-1);
+
+	return (0);
+}
+
 /**
  * compare - compare two cards
- * @card1: a card
- * @card2: a card
+ * @a: a card
+ * @b: a card
  *
  * Return:
- * -1 if card1 is less than card 2
- *  1 if card1 is greater than card2
- *  0 otherwise
+ * an integer < 0 if a < b
+ * an integer > 0 if a > b
+ * 0 otherwise
  */
-int compare(const deck_node_t *card1, const deck_node_t *card2)
+int compare(const void *a, const void *b)
 {
+	static const char * const values[] = {
+		"Ace",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"10",
+		"Jack",
+		"Queen",
+		"King"
+	};
+	int indices[] = {0, 0};
+	const card_t *cards[2];
+
+	cards[0] = (* (const deck_node_t **) a)->card;
+	cards[1] = (* (const deck_node_t **) b)->card;
+
+	if (cards[0]->kind != cards[1]->kind)
+		return (cards[0]->kind - cards[1]->kind);
+
+	while (strcmp(cards[0]->value, values[indices[0]]))
+		++indices[0];
+
+	while (strcmp(cards[1]->value, values[indices[1]]))
+		++indices[1];
+
+	return (indices[0] - indices[1]);
 }
 
 /**
@@ -21,4 +79,27 @@ int compare(const deck_node_t *card1, const deck_node_t *card2)
  */
 void sort_deck(deck_node_t **deck)
 {
+	deck_node_t *card, *cards[DECKSIZE];
+	size_t index = 0;
+
+	if (deck)
+	{
+		for (card = *deck; card; card = card->next)
+			cards[index++] = card;
+
+		qsort(cards, DECKSIZE, sizeof(card), compare);
+
+		for (index = 0; index < DECKSIZE; ++index)
+		{
+			if (index)
+				cards[index]->prev = cards[index - 1];
+			else
+				cards[index]->prev = NULL;
+			if (index + 1 < DECKSIZE)
+				cards[index]->next = cards[index + 1];
+			else
+				cards[index]->next = NULL;
+		}
+		*deck = *cards;
+	}
 }
