98ccac8ac986e228a45f12682e3a9213603eac0e
patrickdeyoreo <pdeyoreo@gmail.com>
2019-11-02 19:17:39 -0700

    Comparing only 1st letter of cards & casting fn pointer

--- 0x1B-sorting_algorithms/1000-sort_deck.c
+++ 0x1B-sorting_algorithms/1000-sort_deck.c
@@ -2,36 +2,10 @@
 #include <stdio.h>
 
 #define DECKSIZE 52
+#define N_VALUES 13
 
 /**
- * strcmp - compare two strings
- * @s1: a string to compare
- * @s2: the other string to compare
- *
- * Return:
- * integer < 0 if s1 < s2,
- * integer > 0 if s1 > s2,
- * otherwise 0
- */
-int strcmp(const char *s1, const char *s2)
-{
-	while (*s1 && *s2)
-	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
-	}
-	if (*s1)
-		return (1);
-	if (*s2)
-		return (-1);
-
-	return (0);
-}
-
-/**
- * compare - compare two cards
+ * cmp_cards - compare two cards
  * @a: a double pointer to a deck node
  * @b: a double pointer to a deck node
  *
@@ -40,36 +14,24 @@ int strcmp(const char *s1, const char *s2)
  * integer > 0 if *a > *b,
  * otherwise 0
  */
-int compare(const void *a, const void *b)
+int cmp_cards(const deck_node_t **a, const deck_node_t **b)
 {
-	static const char * const values[] = {
-		"Ace",
-		"2",
-		"3",
-		"4",
-		"5",
-		"6",
-		"7",
-		"8",
-		"9",
-		"10",
-		"Jack",
-		"Queen",
-		"King"
+	static const char values[N_VALUES] = {
+		'A', '2', '3', '4', '5', '6', '7', '8', '9', '1', 'J', 'Q', 'K'
 	};
 	const card_t *cards[2];
-	unsigned int indices[] = {0, 0};
+	int indices[] = {0, 0};
 
-	cards[0] = (* (const deck_node_t **) a)->card;
-	cards[1] = (* (const deck_node_t **) b)->card;
+	cards[0] = (*a)->card;
+	cards[1] = (*b)->card;
 
 	if (cards[0]->kind != cards[1]->kind)
 		return (cards[0]->kind - cards[1]->kind);
 
-	while (strcmp(cards[0]->value, values[indices[0]]))
+	while (indices[0] < N_VALUES && *cards[0]->value != values[indices[0]])
 		indices[0] += 1;
 
-	while (strcmp(cards[1]->value, values[indices[1]]))
+	while (indices[1] < N_VALUES && *cards[1]->value != values[indices[1]])
 		indices[1] += 1;
 
 	return (indices[0] - indices[1]);
@@ -81,19 +43,24 @@ int compare(const void *a, const void *b)
  */
 void sort_deck(deck_node_t **deck)
 {
-	deck_node_t *cards[DECKSIZE], *card;
+	deck_node_t *cards[DECKSIZE];
+	deck_node_t *card = NULL;
 	size_t index = 0;
 
 	if (deck)
 	{
 		for (card = *deck; card; card = card->next)
 			cards[index++] = card;
-		qsort(cards, DECKSIZE, sizeof(card), compare);
-		cards[index = 0]->prev = NULL;
+
+		qsort(cards, DECKSIZE, sizeof(*cards),
+			(int (*)(const void *, const void *)) cmp_cards);
+
+		index = 0;
+		cards[index]->prev = NULL;
 		while (index < DECKSIZE - 1)
 		{
 			cards[index]->next = cards[index + 1];
-			index += 1;
+			index++;
 			cards[index]->prev = cards[index - 1];
 		}
 		cards[index]->next = NULL;
