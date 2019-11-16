ac152431f4c015ea586e7503b9410e2a44024081
patrickdeyoreo <pdeyoreo@gmail.com>
2019-11-15 16:25:59 -0800

    Adding first attempts at tasks 0x1C 0-5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+def island_perimeter(grid):
+    """Compute the perimeter of an island"""
+    perimeter = 0
+    for y, row in enumerate(grid):
+        for x, value in enumerate(row):
+            if value == 1:
+                for i, j in ((x - 1, y), (x + 1, y)):
+                    if 0 < x < len(row):
+                        if grid[j][i] == 0:
+                            perimeter += 1
+                for i, j in ((x, y - 1), (x, y + 1)):
+                    if 0 < y < len(grid):
+                        if grid[j][i] == 0:
+                            perimeter += 1
+    return perimeter
