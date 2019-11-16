98a73743ceaf7a57eec36427e9530bd073ff1960
patrickdeyoreo <pdeyoreo@gmail.com>
2019-11-15 19:02:37 -0800

    Using try/except in 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,16 +4,24 @@
 
 def island_perimeter(grid):
     """Compute the perimeter of an island"""
-    perimeter = 0
+    perim = 0
     for y, row in enumerate(grid):
         for x, item in enumerate(row):
             if item == 1:
-                if 0 < x:
-                    perimeter += grid[y][x - 1] == 0
-                if x < len(row) - 1:
-                    perimeter += grid[y][x + 1] == 0
-                if 0 < y:
-                    perimeter += grid[y - 1][x] == 0
-                if y < len(grid) - 1:
-                    perimeter += grid[y + 1][x] == 0
-    return perimeter
+                try:
+                    perim += grid[y][x - 1] == 0
+                except IndexError:
+                    pass
+                try:
+                    perim += grid[y][x + 1] == 0
+                except IndexError:
+                    pass
+                try:
+                    perim += grid[y - 1][x] == 0
+                except IndexError:
+                    pass
+                try:
+                    perim += grid[y + 1][x] == 0
+                except IndexError:
+                    pass
+    return perim
