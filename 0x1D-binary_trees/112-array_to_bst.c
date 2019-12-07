1011b9c0914222274bccfc19cc2e08324ff6e4ea
patrickdeyoreo <pdeyoreo@gmail.com>
2019-12-06 17:42:57 -0800

    Trying indexing in 112-array_to_bst.c

--- 0x1D-binary_trees/112-array_to_bst.c
+++ 0x1D-binary_trees/112-array_to_bst.c
@@ -1,20 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - delete an entire binary tree
- * @tree: a pointer to the root node of the tree
- */
-void binary_tree_delete(binary_tree_t *tree)
-{
-	if (tree)
-	{
-		binary_tree_delete(tree->left);
-		binary_tree_delete(tree->right);
-		free(tree);
-	}
-}
-
-/**
  * array_to_bst - build a BST from an array
  * @array: the array from which to construct a BST
  * @size: the size of the array
@@ -25,17 +11,12 @@ void binary_tree_delete(binary_tree_t *tree)
 bst_t *array_to_bst(int *array, size_t size)
 {
 	bst_t *root = NULL;
+	size_t index = 0;
 
 	if (array)
 	{
-		while (size--)
-		{
-			if (!bst_insert(&root, *array++))
-			{
-				binary_tree_delete(root);
-				return (NULL);
-			}
-		}
+		while (index < size)
+			bst_insert(&root, array[index++]);
 	}
 	return (root);
 }
